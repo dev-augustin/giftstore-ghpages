ee46f9f8d2f5cd436c34e6c7fa238acb39cb1262
dev-augustin <dev.augustin00@gmail.com>
2020-06-03 18:56:19 -0500

    Updates

---
+++ precache-manifest.86df997b3512013a0c6392ff9772b25e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e853fd85ac7c08d533a1e8edf3fbe710",
+    "url": "/giftstore-ghpages/index.html"
+  },
+  {
+    "revision": "a2b4b4ee5f171347af5f",
+    "url": "/giftstore-ghpages/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b5f72ff359b3fc831305",
+    "url": "/giftstore-ghpages/static/css/main.048a49c6.chunk.css"
+  },
+  {
+    "revision": "a2b4b4ee5f171347af5f",
+    "url": "/giftstore-ghpages/static/js/2.322af626.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/giftstore-ghpages/static/js/2.322af626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5f72ff359b3fc831305",
+    "url": "/giftstore-ghpages/static/js/main.01818bb2.chunk.js"
+  },
+  {
+    "revision": "13d9ea145d82a6e6e8a3",
+    "url": "/giftstore-ghpages/static/js/runtime-main.bc0a53ee.js"
+  },
+  {
+    "revision": "52d5edd1fb04024a029a75dcc5942ebc",
+    "url": "/giftstore-ghpages/static/media/birthday-table.52d5edd1.jpeg"
+  },
+  {
+    "revision": "d4b56f62a4ee41d247ad2f3d95f7a97e",
+    "url": "/giftstore-ghpages/static/media/biscuits.d4b56f62.jpeg"
+  },
+  {
+    "revision": "d8b9962980663ab0ab61c80d2708ab56",
+    "url": "/giftstore-ghpages/static/media/flowers.d8b99629.jpeg"
+  }
+]);
