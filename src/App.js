8bf644769d319f40a4b01ee4ad793742fa05671c
dev-augustin <dev.augustin00@gmail.com>
2020-06-03 19:23:12 -0500

    added hash router in navigation

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import ThankYou from  './components/ThankYou'
 
 function App() {
   return (
-    <Router  basename='/'>
+    <Router  basename={process.env.PUBLIC_URL}>
        <div>
          <Navigation/>
          <Switch>
