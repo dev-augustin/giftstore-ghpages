a60a2543698e20d3e646c3f5e050c3184f425214
dev-augustin <dev.augustin00@gmail.com>
2020-06-03 19:15:17 -0500

    added basename

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import ThankYou from  './components/ThankYou'
 
 function App() {
   return (
-    <Router basename='/giftstore-ghpages'>
+    <Router basename={process.env.PUBLIC_URL}>
        <div>
          <Navigation/>
          <Switch>
