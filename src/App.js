fba9f14926379ff67756f259a197c01950159c78
dev-augustin <dev.augustin00@gmail.com>
2020-06-03 19:11:34 -0500

    added basename

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import ThankYou from  './components/ThankYou'
 
 function App() {
   return (
-    <Router basename='/'>
+    <Router basename='/giftstore-ghpages'>
        <div>
          <Navigation/>
          <Switch>
